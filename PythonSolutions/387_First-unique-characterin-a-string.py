7b0526313450d236a654960471454db027c9c434
Yan Chen <59455817+whitefoxcy@users.noreply.github.com>
2021-08-12 14:53:13 -0700

    Update 387_First-unique-characterin-a-string.py

--- PythonSolutions/387_First-unique-characterin-a-string.py
+++ PythonSolutions/387_First-unique-characterin-a-string.py
@@ -4,3 +4,14 @@ Created on Thu Aug 12 11:07:46 2021
 
 @author: yan_chen
 """
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        
+        count = collections.Counter(s)
+        
+        for i, v in enumerate(s):
+            if count[v] == 1:
+                return i
+            
+        return -1
+        
